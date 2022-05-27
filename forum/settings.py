79f1c35e698d77e6efcddf8ce1388f9d6f69665a
hoxas <hoxas@live.com>
2022-05-27 17:30:08 -0300

    👃 Adding django_nose & configs to settings

--- forum/settings.py
+++ forum/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
 
     'main.apps.MainConfig',
     'user.apps.UserConfig',
+
+    'django_nose',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+NOSE_ARGS = ['--with-spec', '--spec-color', '--nocapture']
