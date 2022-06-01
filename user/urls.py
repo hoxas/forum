497db9d46a2dbd94235d9e1acc60a92d50b139fb
hoxas <hoxas@live.com>
2022-05-31 21:41:56 -0300

    ⛓️ Adding new urls

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.account, name='account'),
+    path('auth/', views.auth, name='auth'),
+    path('register/', views.register_POST, name='register'),
+    path('login/', views.login_POST, name='login'),
     path('<str:profile_displayname>/', views.account, name='account-profile'),
+
 ]
