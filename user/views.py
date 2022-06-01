ff8bfdecc9bc64367f729c20214f3681bdca6d49
hoxas <hoxas@live.com>
2022-05-31 21:42:28 -0300

    🐗 Adding views login and register

--- user/views.py
+++ user/views.py
@@ -1,10 +1,55 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
 
 from user.utils import *
+from user.models import Profile
 
-from user.forms import SignUpForm
+from user.forms import SignUpForm, AuthenticationForm, UserChangeForm
 
 # Create your views here.
+
+
+def auth(request):
+    data = Request_Context(request)
+
+    if request.user.is_authenticated:
+        return redirect('/')
+    else:
+        return render(request, 'user/auth.html', {'data': data, 'AuthenticationForm': AuthenticationForm, 'SignUpForm': SignUpForm})
+
+
+def register_POST(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            Profile.objects.create(
+                user=user, displayname=form.cleaned_data['displayname'])
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Account created successfully.')
+            return redirect('/')
+        messages.error(request, 'Error creating account.')
+        return redirect('/account/auth/')
+
+    return redirect('/account/auth/')
+
+
+def login_POST(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Logged in as {username}')
+                return redirect('/')
+
+    messages.error(request, 'Invalid username or password.')
+
+    return redirect('/account/auth/')
 
 
 def account(request, profile_displayname=False):
@@ -15,4 +60,4 @@ def account(request, profile_displayname=False):
     elif data.profile:
         return redirect('/account/' + data.profile + '/')
     else:
-        return render(request, 'user/auth.html', {'data': data, 'SignUpForm': SignUpForm})
+        return redirect('/account/auth/')
