f2c72773106882a89668c9474997da12ecd1d49c
hoxas <hoxas@live.com>
2022-06-01 21:51:37 -0300

    🦺 Adding tests to user.utils

---
+++ user/tests/tests_utils.py
@@ -0,0 +1,42 @@
+from tests.utils import *
+
+from user.utils import *
+
+"""Testing user.utils"""
+
+
+class TestRequestContext(TestCase):
+    """Testing user.utils.Request_Context"""
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.request = RequestFactory().get('/')
+        objectCreator(cls, 'comment')
+        cls.unauth_user = FakeUnauthUser()
+
+    def test_request_context_properties(self):
+        """Testing user.utils.RequestContext properties"""
+
+        self.request.user = self.unauth_user
+
+        data_unauthenticated = Request_Context(self.request)
+        # Super class properties
+        self.assertEqual(data_unauthenticated.request, self.request)
+        self.assertEqual(data_unauthenticated.user, self.unauth_user)
+
+        # Sub class properties
+        self.assertFalse(data_unauthenticated.profile)
+        self.assertFalse(data_unauthenticated.profile_search)
+
+        self.request.user = self.user
+
+        data_authenticated = Request_Context(self.request)
+        self.assertFalse(data_authenticated.profile_search)
+        self.assertEqual(data_authenticated.profile, self.profile.displayname)
+
+        data_authenticated_profile_search = Request_Context(
+            self.request, profile=self.profile.displayname)
+
+        self.assertFalse(data_authenticated_profile_search.profile)
+        self.assertEqual(
+            data_authenticated_profile_search.profile_search, self.profile)
