32b161128b369e3cd5acf75b108abfcb55b9ccef
hoxas <hoxas@live.com>
2022-06-06 01:51:35 -0300

    🎀 + Create comment view

--- main/views.py
+++ main/views.py
@@ -17,20 +17,6 @@ def category(request, category_name):
     return render(request, 'main/index.html', {'data': data})
 
 
-def post(request, category_name, post_id):
-    data = Request_Context(request, category=category_name, post_id=post_id)
-    data.post.views += 1
-    data.post.save()
-
-    if request.user.is_authenticated:
-        if request.user.profile in data.post.views_unique.all():
-            pass
-        else:
-            data.post.views_unique.add(request.user.profile)
-
-    return render(request, 'main/post.html', {'data': data, 'post': data.post})
-
-
 def create_post_POST(request):
     data = Request_Context(request)
     if request.method == 'POST':
@@ -51,3 +37,41 @@ def create_post_POST(request):
         messages.error(request, 'Error creating post.',
                        extra_tags='add_post')
         return redirect('/')
+
+
+def post(request, category_name, post_id):
+    data = Request_Context(request, category=category_name, post_id=post_id)
+    data.post.views += 1
+    data.post.save()
+
+    if request.user.is_authenticated:
+        if request.user.profile in data.post.views_unique.all():
+            pass
+        else:
+            data.post.views_unique.add(request.user.profile)
+
+    return render(request, 'main/post.html', {'data': data, 'post': data.post, 'CommentForm': CommentForm})
+
+
+def create_comment_POST(request, category_name, post_id):
+    if request.method == 'POST':
+        data = Request_Context(
+            request, category=category_name, post_id=post_id)
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = data.post
+            comment.profile = data.profile
+            comment.save()
+            messages.success(
+                request, 'Comment created successfully.', extra_tags='main')
+            return redirect(f'/category/{category_name}/{post_id}/')
+        elif form.errors:
+            for error in form.errors.values():
+                messages.error(request,  error,
+                               extra_tags='add_comment')
+            return redirect(f'/category/{category_name}/{post_id}/')
+
+        messages.error(request, 'Error creating comment.',
+                       extra_tags='add_comment')
+        return redirect(f'/category/{category_name}/{post_id}/')
