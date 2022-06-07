2894541421245b5889cbbc7c5450600f256234a4
hoxas <hoxas@live.com>
2022-06-07 00:41:29 -0300

    🖼️ + Paginate utility class #15

--- main/utils.py
+++ main/utils.py
@@ -2,12 +2,16 @@ from common.utils import *
 from django.core.paginator import Paginator
 
 
-def paginate(request, objects, per_page=20):
-    paginator = Paginator(objects, per_page)
-    page = request.GET.get('page')
-    objects = paginator.get_page(page)
-
-    return objects
+class Paginate:
+    def __init__(self, request, objects, per_page=20):
+        self.paginator = Paginator(objects, per_page)
+        self.pagenumber = request.GET.get('page')
+        if self.pagenumber is None:
+            self.pagenumber = 1
+        self.objects = self.paginator.get_page(self.pagenumber)
+        get_elided_page_range = self.paginator.get_elided_page_range(
+            self.pagenumber)
+        self.elided_page_range = [page for page in get_elided_page_range]
 
 
 class Request_Context(Request_Context_Generic):
@@ -24,13 +28,19 @@ class Request_Context(Request_Context_Generic):
                 self.post = Post.objects.get(id=kwargs['post_id'])
                 comments = Comment.objects.filter(
                     post=self.post).order_by('created_on')
-                self.comments = paginate(request, comments)
+                pagination = Paginate(request, comments)
+                self.page_range = pagination.elided_page_range
+                self.comments = pagination.objects
             else:
                 posts = Post.objects.filter(
                     category=self.category).order_by('-created_on')
-                self.posts = paginate(request, posts)
+                pagination = Paginate(request, posts)
+                self.page_range = pagination.elided_page_range
+                self.posts = pagination.objects
         else:
             posts = Post.objects.all().order_by('-created_on')
-            self.posts = paginate(request, posts)
+            pagination = Paginate(request, posts)
+            self.page_range = list(pagination.elided_page_range)
+            self.posts = pagination.objects
 
     # So basically if a post has no category it can't be accessed?
