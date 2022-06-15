b86317ad0f28285cacd08ce076103537b3e006c7
hoxas <hoxas@live.com>
2022-06-15 07:20:15 -0300

    🏵️ Implementing registration and login tests #10

--- user/tests/tests_views.py
+++ user/tests/tests_views.py
@@ -1,5 +1,7 @@
 from tests.utils import *
 
+from django.core.exceptions import ObjectDoesNotExist
+
 from user.views import *
 
 
@@ -10,9 +12,8 @@ class TestViews(TestCase):
     def setUpTestData(cls):
         cls.client = Client()
         objectCreator(cls, 'profile')
-        cls.authenticatedClient = Client().login({'username': cls.user.username,
-                                                 'password': data.password}, follow=True)
-        print(cls.authenticatedClient)
+        cls.authenticatedClient = Client()
+        cls.authenticatedClient.force_login(cls.user, backend=None)
 
     def test_auth_GET(self):
         """Requesting auth page"""
@@ -22,3 +23,40 @@ class TestViews(TestCase):
 
         authresponse = self.authenticatedClient.get(reverse('auth'))
         self.assertRedirects(authresponse, '/')
+
+    def test_register_POST(self):
+        """Registering new user"""
+        # Making sure models cascade is working
+        User.objects.get(id=self.user.id).delete()
+        self.assertRaises(ObjectDoesNotExist,
+                          lambda: User.objects.get(id=self.user.id))
+        self.assertRaises(ObjectDoesNotExist,
+                          lambda: Profile.objects.get(id=self.profile.id))
+
+        response = self.client.post(reverse('register'))
+        self.assertRedirects(response, reverse('auth'))
+
+        response = self.client.post(reverse('register'), data={
+            'username': data.username,
+            'displayname': data.displayname,
+            'email': data.email,
+            'password1': data.password,
+            'password2': data.password
+        })
+        # print(response.context)
+
+        self.assertRedirects(response, reverse('index'))
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
+
+    def test_login_POST(self):
+        """Logging in user"""
+        response = self.client.post(reverse('login'))
+        self.assertRedirects(response, reverse('auth'))
+
+        response = self.client.post(reverse('login'), data={
+            'username': data.username,
+            'password': data.password
+        })
+        print(response.context)
+        self.assertRedirects(response, reverse('index'))
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
